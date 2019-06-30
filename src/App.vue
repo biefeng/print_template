d17c5e9b19cac05da50a328a983358ad172570da
335049213@qq.com <biefeng123>
2019-07-01 01:48:37 +0800

    完成基本拖拽

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
         :value="item.value">
       </el-option>
     </el-select>
+    <el-button type="primary" @click="appendElement">主要按钮</el-button>
     <div id="templateArea" style="position: absolute;left:800px;width: 450px;height: 600px;border: 1px solid red;">
       <img id="dragImage" style="left: 100px;position: absolute" @dragstart="ondragstart" @drag="ondrag"
            @dragend="ondragend" src="./assets/logo.png" alt=""/>
@@ -27,16 +28,16 @@
     data() {
       return {
         options: [{
-          value: '文本',
+          value: 'text',
           label: '文本'
         }, {
-          value: '二维码',
+          value: 'qrCode',
           label: '二维码'
         }, {
-          value: '条形码',
+          value: 'barCode',
           label: '条形码'
         }, {
-          value: '图片',
+          value: 'img',
           label: '图片'
         }],
         contentType: '',
@@ -51,36 +52,55 @@
     },
     methods: {
       ondragstart(e) {
-        console.log('事件源p3开始拖动');
+        alert(1)
         //记录刚一拖动时，鼠标在飞机上的偏移量
         this.dragImage.offsetX = e.offsetX;
         this.dragImage.offsetY = e.offsetY;
       },
       ondrag(e) {
-        console.log('事件源p3拖动中');
         let templateAreaDiv = document.getElementById("templateArea");
         var x = e.pageX;
         var y = e.pageY;
-        console.log(x + '-' + y);
         //drag事件最后一刻，无法读取鼠标的坐标，pageX和pageY都变为0
         if (x == 0 && y == 0) {
           return; //不处理拖动最后一刻X和Y都为0的情形
         }
-        const offsetLeft= templateAreaDiv.offsetLeft
-        const offsetTop=templateAreaDiv.offsetTop
-        x =x- this.dragImage.offsetX-offsetLeft;
-        if (x<0)
-          x=0
-        y =y- this.dragImage.offsetY-offsetTop;
-        if(y<0)
-          y=0
-
+        const leftBoundary = templateAreaDiv.offsetLeft
+        const topBoundary = templateAreaDiv.offsetTop
+        x = x - this.dragImage.offsetX - leftBoundary;
+        if (x < 0)
+          x = 0
+        y = y - this.dragImage.offsetY - topBoundary;
+        if (y < 0)
+          y = 0
         let dragImageDiv = document.getElementById("dragImage");
+        if (y > templateAreaDiv.offsetHeight - dragImageDiv.offsetHeight) {
+          y = templateAreaDiv.offsetHeight - dragImageDiv.offsetHeight
+        }
+        if (x > templateAreaDiv.offsetWidth - dragImageDiv.offsetWidth) {
+          x = templateAreaDiv.offsetWidth - dragImageDiv.offsetWidth
+        }
         dragImageDiv.style.left = x + 'px';
         dragImageDiv.style.top = y + 'px';
       },
       ondragend() {
-        console.log('源对象p3拖动结束');
+
+      },
+      appendElement() {
+        if (!this.contentType || this.contentType === '') {
+          this.$message.warning("请选择一种类型")
+          return
+        }
+        let templateAreaDiv = document.getElementById("templateArea")
+        let childElement
+        switch (this.contentType) {
+          case "text":
+            childElement = document.createElement("p", {type: "",ondragstart:this.ondragstart});
+            childElement.textContent="你好"
+            childElement.ondragstart=this.ondragstart
+            break;
+        }
+        templateAreaDiv.appendChild(childElement)
       }
     }
   }
