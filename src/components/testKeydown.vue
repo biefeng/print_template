ccafc0020826ba6ffa952cd3b182bf436a3b04b3
BieFeNg <feng.bie@myt11.com>
2019-07-10 10:37:27 +0800

    通过mousemove实现拖拽

--- src/components/testKeydown.vue
+++ src/components/testKeydown.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div id="moved" style="background-color: darkgrey;width: 100px;height: 100px" ></div>
+  <div id="container" style="position: absolute">
+    <div id="moved" style="background-color: darkgrey;width: 100px;height: 100px"></div>
 
   </div>
 </template>
@@ -8,9 +8,37 @@
 <script>
   export default {
     name: "testKeydown",
-    mounted(){
+    data() {
+      return {
+        position: {
+          currentPositionX: 0,
+          currentPositionY: 0,
+          currentCursorPositionX: 0,
+          currentCursorPositionY: 0
+        }
+      }
+    },
+    methods: {
+      moveElement(e) {
+        let moved = document.getElementById("moved");
+        moved.style.transform = "translate(" + (e.pageX - this.position.currentCursorPositionX + this.position.currentPositionX) + "px," + (e.pageY - this.position.currentCursorPositionY + this.position.currentPositionY) + "px)"
+        console.log(e)
+      },
+      bindEvent(e) {
+        document.addEventListener("mousemove", this.moveElement)
+        this.position.currentCursorPositionX = e.pageX
+        this.position.currentCursorPositionY = e.pageY
+      },
+      removeEvent(e) {
+        this.position.currentPositionX = e.pageX - this.position.currentCursorPositionX + this.position.currentPositionX
+        this.position.currentPositionY = e.pageY - this.position.currentCursorPositionY + this.position.currentPositionY
+        document.removeEventListener("mousemove", this.moveElement)
+      }
+    },
+    mounted() {
       let moved = document.getElementById("moved");
-      moved.onkeydown=this.moveByDirectKey
+      moved.addEventListener("mousedown", this.bindEvent)
+      moved.addEventListener("mouseup", this.removeEvent)
     }
   }
 </script>
